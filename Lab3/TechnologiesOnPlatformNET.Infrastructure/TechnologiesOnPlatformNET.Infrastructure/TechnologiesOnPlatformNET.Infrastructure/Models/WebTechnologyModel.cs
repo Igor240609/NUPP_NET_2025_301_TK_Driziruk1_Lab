0d9c22b6acd90929b25ff3eae4794efccd6fa93f
Igor <idiriziruk151@gmail.com>
2025-05-17 23:43:22 +0300

    Lab

---
+++ Lab3/TechnologiesOnPlatformNET.Infrastructure/TechnologiesOnPlatformNET.Infrastructure/TechnologiesOnPlatformNET.Infrastructure/Models/WebTechnologyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TechnologiesOnPlatformNET.Infrastructure.Models
+{
+    public class WebTechnologyModel
+    {
+        public Guid Id { get; set; }
+        public string FrontendFramework { get; set; }
+        public bool IsCloudReady { get; set; }
+
+        public Guid DotNetTechnologyId { get; set; }
+        public DotNetTechnologyModel DotNetTechnology { get; set; }
+
+        public AspNetCoreModel AspNetCore { get; set; }
+    }
+
+}
